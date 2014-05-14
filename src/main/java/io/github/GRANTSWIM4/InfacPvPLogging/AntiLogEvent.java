009fc0339e588a13e4bffcce3dcfbfaed214abbd
Jan <janie177@hotmail.com>
2014-05-14 16:09:05 +0200

    Update AntiLogEvent.java
    Some checks to prevent NPE's.

--- src/main/java/io/github/GRANTSWIM4/InfacPvPLogging/AntiLogEvent.java
+++ src/main/java/io/github/GRANTSWIM4/InfacPvPLogging/AntiLogEvent.java
@@ -29,33 +29,51 @@ public class AntiLogEvent implements Listener {
 
 		@EventHandler
 		public void entityDamageByEntityEvent(EntityDamageByEntityEvent event){
-
-			final Player attacker = (Player) event.getDamager();
-			final Player attacked = (Player) event.getEntity();
-			if(event.getDamager() instanceof Player && event.getEntity() instanceof Player){
-
-				if(!(inCombat.contains(attacker))){
-
-					inCombat.add(attacker);
-					inCombat.add(attacked);
-					attacker.sendMessage(ChatColor.RED+"You are now in combat with " + attacked.getName());
-					attacked.sendMessage(ChatColor.RED+"You are now in combat with " + attacker.getName());	
-
-
-					plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, new Runnable() {
-
-						@Override
-						public void run() {
-							attacker.sendMessage(ChatColor.RED+"You are no longer in combat!");
-							attacked.sendMessage(ChatColor.RED+"You are no longer in combat!");
-
-							inCombat.remove(attacker);
-							inCombat.remove(attacked);
-
-						}
-					},  plugin.getConfig().getLong("Time_In_Combat") * 20);
-				}     
+			
+			final Player attacker;
+			final Player attacked;
+			
+			if(!event.getEntity() instanceof Player)return;
+			if(!(event.getDamager() instanceof Player) || !(event.getDamager() instanceof Arrow) return;
+			if(event.getDamager() instanceof Arrow)
+			{
+				Arrow arrow = (Arrow) event.getDamager();
+				//Do some code here to get who fired the arrow.
+				//Make sure to check wether the shooter is a player too!
+				attacker = (Player) arrow.getshooter();
+				//Idk if this method exists. Just make it "attacker = shooter" where shooter is a players.
+				//So check wether it is. 
 			}
+			
+			if(event.getDamager() instanceof Player)
+			{
+				attacker = (Player) event.getDamager();		
+			}
+			
+			if(attacker == null)return;
+			attacked = (Player) event.getEntity();
+
+			if(!(inCombat.contains(attacker))){
+
+				inCombat.add(attacker);
+				inCombat.add(attacked);
+				attacker.sendMessage(ChatColor.RED+"You are now in combat with " + attacked.getName());
+				attacked.sendMessage(ChatColor.RED+"You are now in combat with " + attacker.getName());	
+
+
+				plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, new Runnable() {
+
+					@Override
+					public void run() {
+						attacker.sendMessage(ChatColor.RED+"You are no longer in combat!");
+						attacked.sendMessage(ChatColor.RED+"You are no longer in combat!");
+
+						inCombat.remove(attacker);
+						inCombat.remove(attacked);
+
+					}
+				},  plugin.getConfig().getLong("Time_In_Combat") * 20);
+			}     
 		}
 
 		@EventHandler
@@ -64,7 +82,7 @@ public class AntiLogEvent implements Listener {
 			if(inCombat.contains(player)){
 				if (inCombat.contains(player)) {
 					CompleteDossier dossier = Infractions.getCompleteDossier(player.getName());
-					dossier.cite(new Infraction(player.getUniqueId(), System.currentTimeMillis(), "PvP Loged", 5, new Issuer(IssuerType.CUSTOM, "PvPLogPlugin")));
+					dossier.cite(new Infraction(player.getUniqueId(), System.currentTimeMillis(), "PvP Logged", 5, new Issuer(IssuerType.CUSTOM, "PvPLogPlugin")));
 				}
 			}
 		}
