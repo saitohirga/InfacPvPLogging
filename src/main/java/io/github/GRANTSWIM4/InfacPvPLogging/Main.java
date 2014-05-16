6de9d8f98a4ba294659d4348e006816ba3bf7ef4
HmmmQuestionMark <ajchauncey@gmail.com>
2014-05-16 13:38:34 -0400

    Update Main.java

--- src/main/java/io/github/GRANTSWIM4/InfacPvPLogging/Main.java
+++ src/main/java/io/github/GRANTSWIM4/InfacPvPLogging/Main.java
@@ -2,32 +2,44 @@ package io.github.GRANTSWIM4.InfacPvPLogging;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin
+{
+	private boolean infractionsInstalled;
+	
+	/**
+	 * Bukkit enable.
+	 */
  	@Override
-	public void onEnable() {
-		if (getServer().getPluginManager().getPlugin("Infractions") != null) {
-			getLogger().info(
-					"Protecting your server form PvP logger since 2014!");
-	        getServer().getPluginManager().registerEvents(new AntiLogEvent(), this);
+	public void onEnable()
+	{
+		this.infractionsInstalled = getServer().getPluginManager().getPlugin("Infractions") != null;
+		if (this.infractionsInstalled)
+		{
+			// Welcome message
+			getLogger().info("Protecting your server form PvP logger since 2014!");
+			
+			// Register events
+	        	getServer().getPluginManager().registerEvents(new AntiLogEvent(this), this);
+	        	
+	        	// Handle config
 			this.saveDefaultConfig();
-		    this.getConfig().options().copyDefaults(true);
-		} else {
-			getLogger()
-					.info("You don't have Infractions get it at http://dev.bukkit.org/bukkit-plugins/infractions/ Bye bye ");
+		    	this.getConfig().options().copyDefaults(true);
+		}
+		else
+		{
+			// Can't run without Infractions installed
+			getLogger().info("You don't have Infractions get it at http://dev.bukkit.org/bukkit-plugins/infractions/ Bye bye ");
 			getServer().getPluginManager().disablePlugin(this);
-			return;
 		}
 	}
+	
+	/**
+	 * Bukkit disable.
+	 */
 	@Override
-	public void onDisable() {
-		if (getServer().getPluginManager().getPlugin("Infractions") != null) {
-			getLogger().info("Its been a honor protecting your server Bye!");
-		}
-		 else {
-			getLogger().info("It will be a honnor protecting when you setup up everything");
-
+	public void onDisable()
+	{
+		// Goodbye message
+		getLogger().info(this.infractionsInstalled ? "It's been a honor protecting your server Bye!" : "It will be a honor protecting when you setup up everything");
 	}
-
-}
-
 }
