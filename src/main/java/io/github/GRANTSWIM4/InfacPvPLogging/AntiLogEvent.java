fa6b59371626c2efaba61006e0382d816b067b12
HmmmQuestionMark <ajchauncey@gmail.com>
2014-05-16 13:33:25 -0400

    Update AntiLogEvent.java

--- src/main/java/io/github/GRANTSWIM4/InfacPvPLogging/AntiLogEvent.java
+++ src/main/java/io/github/GRANTSWIM4/InfacPvPLogging/AntiLogEvent.java
@@ -18,59 +18,69 @@ import com.censoredsoftware.infractions.bukkit.issuer.Issuer;
 import com.censoredsoftware.infractions.bukkit.issuer.IssuerType;
 
 
+/**
+ * Listener class.
+ */
+public class AntiLogEvent implements Listener
+{
+	/**
+	 * Constructor
+	 */
+	AntiLogEvent(Plugin plugin)
+	{
+		this.plugin = plugin;
+	}
 
-
-public class AntiLogEvent implements Listener {
-
-	 
+	/**
+	 * Quit event.
+	 */
 	@EventHandler
-	  public void onAntiLogQuit(PlayerQuitEvent event)
-	  {
-	    Player p = event.getPlayer();
-	    if (this.antilog.contains(p.getName())) {
-	      Bukkit.getServer().broadcastMessage(ChatColor.GRAY + p.getName() + " has combat logged");
-	      CompleteDossier dossier = Infractions.getCompleteDossier(p.getName());
+	public void onAntiLogQuit(PlayerQuitEvent event)
+	{
+		Player p = event.getPlayer();
+		if (this.antilog.contains(p.getName()))
+		{
+			Bukkit.getServer().broadcastMessage(ChatColor.GRAY + p.getName() + " has combat logged");
+			CompleteDossier dossier = Infractions.getCompleteDossier(p.getName());
 			dossier.cite(new Infraction(p.getUniqueId(), System.currentTimeMillis(), "PvP Logged", 5, new Issuer(IssuerType.CUSTOM, "PvPLogPlugin")));
-	    }
-	  }
+		}
+	}
 
+	/**
+	 * Damage by entity event.
+	 */
+	@EventHandler
+	public void onAntiLogDmg(EntityDamageByEntityEvent event) 
+	{
+	  	if (((event.getDamager() instanceof Player)) && ((event.getEntity() instanceof Player)))
+	    	{
+			final Player player = (Player)event.getEntity();
+		    	final Player target = (Player)event.getDamager();
+	
+			if ((!this.antilog.contains(player.getName())) && (!this.antilog.contains(target.getName())))
+			{
+			        this.antilog.add(player.getName());
+			        this.antilog.add(target.getName());
+			        player.sendMessage(ChatColor.GOLD + "You're now in Combat!");
+			        target.sendMessage(ChatColor.GOLD + "You're now in Combat!");
+			        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this.plugin, new Runnable()
+			        {
+					public void run()
+			        	{
+			          		if ((antilog.contains(player.getName())) && (antilog.contains(target.getName())))
+			          		{
+			           			antilog.remove(player.getName());
+			           			antilog.remove(target.getName());
+			           			target.sendMessage(ChatColor.GREEN + "You can now log out safely.");
+			           			player.sendMessage(ChatColor.GREEN + "You can now log out safely.");
+			          		}
+			        	}
+			      	} , 1000L);
+		    	}
+		}
+	}
 
-@EventHandler
-public void onAntiLogDmg(EntityDamageByEntityEvent event) {
-  if (((event.getDamager() instanceof Player)) && 
-    ((event.getEntity() instanceof Player))) {
-    final Player player = (Player)event.getEntity();
-    final Player target = (Player)event.getDamager();
-
-    if ((!this.antilog.contains(player.getName())) && 
-      (!this.antilog.contains(target.getName()))) {
-      this.antilog.add(player.getName());
-      this.antilog.add(target.getName());
-      player.sendMessage(ChatColor.GOLD + "You're now in Combat!");
-      target.sendMessage(ChatColor.GOLD + "You're now in Combat!");
-      Bukkit.getServer().getScheduler().scheduleSyncDelayedTask((Plugin) this, new Runnable() {
-        public void run() {
-          if ((antilog.contains(player.getName())) &&
-            (antilog.contains(target.getName()))) {
-           antilog.remove(player.getName());
-           antilog.remove(target.getName());
-           target.sendMessage(ChatColor.GREEN + "You can now log out safely.");
-           player.sendMessage(ChatColor.GREEN + "You can now log out safely.");
-          }
-        }
-      }
-      , 1000L);
-    }
-  }
+	// Variables
+	private List<String> antilog = new ArrayList<String>();
+	private Plugin plugin;
 }
-
-
-
-
-
-public ArrayList<String> antilog = new ArrayList<String>();
-
-}
-
-
-
