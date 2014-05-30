b331d801445d13e6d8557b2147104da8e9fed051
Grant Baron <granthbaron@aol.com>
2014-05-30 13:54:17 -0400

    Formated code

--- src/main/java/io/github/GRANTSWIM4/InfacPvPLogging/AntiLogEvent.java
+++ src/main/java/io/github/GRANTSWIM4/InfacPvPLogging/AntiLogEvent.java
@@ -48,55 +48,61 @@ public class AntiLogEvent implements Listener {
 					IssuerType.CUSTOM, "PvPLogPlugin")));
 		}
 	}
-		
-	
 
 	/**
 	 * Damage by entity event.
 	 */
 	@EventHandler
-	
 	public void onAntiLogDmg(EntityDamageByEntityEvent event) {
 
-			if (event.getDamager() instanceof Arrow) {
-				Arrow arrow = (Arrow) event.getDamager();
-				if (arrow.getShooter() instanceof Player && event.getEntity() instanceof Player) {
-						final Player Player = (Player) arrow.getShooter();
-						final Player Target = (Player) event.getEntity();
-		if (((event.getDamager() instanceof Player)))
-			if (((event.getDamager() instanceof Player) && !event.isCancelled())
-					&& ((event.getEntity() instanceof Player))) {
-				final Player Player = (Player) event.getEntity();
-				final Player Target = (Player) event.getDamager();
-			
-				
+		if (event.getDamager() instanceof Arrow) {
+			Arrow arrow = (Arrow) event.getDamager();
+			if (arrow.getShooter() instanceof Player
+					&& event.getEntity() instanceof Player) {
+				final Player Player = (Player) arrow.getShooter();
+				final Player Target = (Player) event.getEntity();
+				if (((event.getDamager() instanceof Player)))
+					if (((event.getDamager() instanceof Player) && !event
+							.isCancelled())
+							&& ((event.getEntity() instanceof Player))) {
+						final Player Player = (Player) event.getEntity();
+						final Player Target = (Player) event.getDamager();
 
-				if ((!this.antilog.contains(Player.getName()))
-						&& (!this.antilog.contains(Target.getName()))) {
-					this.antilog.add(Player.getName());
-					this.antilog.add(Target.getName());
-					Player.sendMessage(ChatColor.GOLD + "You're now in Combat!");
-					Target.sendMessage(ChatColor.GOLD + "You're now in Combat!");
-					Bukkit.getServer()
-							.getScheduler()
-							.scheduleSyncDelayedTask(this.plugin,
-									new Runnable() {
-										public void run() {
-											if ((antilog.contains(Player
-													.getName()))
-													&& (antilog.contains(Target
-															.getName()))) {
-												antilog.remove(Player.getName());
-												antilog.remove(Target.getName());
-												Target.sendMessage(ChatColor.GREEN
-														+ "You can now log out safely.");
-												Player.sendMessage(ChatColor.GREEN
-														+ "You can now log out safely.");
-												
-											}
-										}
-												}, 1000L); } } }
-					
+						if ((!this.antilog.contains(Player.getName()))
+								&& (!this.antilog.contains(Target.getName()))) {
+							this.antilog.add(Player.getName());
+							this.antilog.add(Target.getName());
+							Player.sendMessage(ChatColor.GOLD
+									+ "You're now in Combat!");
+							Target.sendMessage(ChatColor.GOLD
+									+ "You're now in Combat!");
+							Bukkit.getServer()
+									.getScheduler()
+									.scheduleSyncDelayedTask(this.plugin,
+											new Runnable() {
+												public void run() {
+													if ((antilog
+															.contains(Player
+																	.getName()))
+															&& (antilog
+																	.contains(Target
+																			.getName()))) {
+														antilog.remove(Player
+																.getName());
+														antilog.remove(Target
+																.getName());
+														Target.sendMessage(ChatColor.GREEN
+																+ "You can now log out safely.");
+														Player.sendMessage(ChatColor.GREEN
+																+ "You can now log out safely.");
+
+													}
+												}
+											}, 1000L);
+						}
+					}
+			}
+
+		}
+	}
 }
-}}
-	
